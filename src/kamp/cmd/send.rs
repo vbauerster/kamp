e2eae26bd0619c06ef2bc2f439d9bc27b6bd7ed7
Vladimir Bauer <vbauerster@gmail.com>
2022-03-22 19:59:36 +0500

    send cmd: &str

--- src/kamp/cmd/send.rs
+++ src/kamp/cmd/send.rs
@@ -1,7 +1,7 @@
 use super::Context;
 use super::Error;
 
-pub(crate) fn send(ctx: Context, buffers: Vec<String>, cmd: String) -> Result<(), Error> {
+pub(crate) fn send(ctx: Context, buffers: Vec<String>, cmd: &str) -> Result<(), Error> {
     let cmd = format!("  {}\n", cmd);
     ctx.send(&cmd, super::to_csv_buffers(buffers)).map(|_| ())
 }
