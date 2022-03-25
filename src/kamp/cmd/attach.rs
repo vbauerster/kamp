b170bb978c095a292770cb169913a125e40c1ff3
Vladimir Bauer <vbauerster@gmail.com>
2022-03-26 01:06:41 +0500

    attach -b: delete *stdin.. buffer

--- src/kamp/cmd/attach.rs
+++ src/kamp/cmd/attach.rs
@@ -4,6 +4,8 @@ use super::Error;
 pub(crate) fn attach(ctx: &Context, buffer: Option<String>) -> Result<(), Error> {
     let mut cmd = String::new();
     if let Some(buffer) = buffer {
+        cmd.push_str(r#"eval %sh{ case $kak_bufname in (\*stdin*) echo delete-buffer ;; esac }"#);
+        cmd.push_str("\n");
         cmd.push_str("buffer '");
         cmd.push_str(&buffer);
         cmd.push_str("'");
