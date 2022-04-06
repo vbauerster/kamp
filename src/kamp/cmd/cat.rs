0f7158a7266134c96490068b409252f4494b2ee3
Vladimir Bauer <vbauerster@gmail.com>
2022-04-06 22:20:05 +0500

    readable options check

--- src/kamp/cmd/cat.rs
+++ src/kamp/cmd/cat.rs
@@ -2,7 +2,7 @@ use super::Context;
 use super::Error;
 
 pub(crate) fn cat(ctx: &Context, buffer: Option<String>) -> Result<String, Error> {
-    if ctx.client.as_deref().or(buffer.as_deref()).is_none() {
+    if ctx.client.is_none() && buffer.is_none() {
         return Err(Error::InvalidContext);
     }
     ctx.send("write %opt{kamp_out}", buffer)
