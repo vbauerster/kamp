6033f011ecbd2c7b5de39b9b0596425bb4b1d4b3
Vladimir Bauer <vbauerster@gmail.com>
2022-04-16 15:10:02 +0500

    avoid heap alloc by using array instead of Vec

--- src/kamp/cmd/edit.rs
+++ src/kamp/cmd/edit.rs
@@ -9,19 +9,19 @@ use super::Error;
 pub(crate) fn edit(ctx: &Context, files: Vec<String>) -> Result<(), Error> {
     let mut buf = String::new();
     let mut coord_buf = String::new();
-    let mut tmp = Vec::new();
+    let mut pair = [None, None];
     let mut i = 0;
 
     for item in files.iter().take(2) {
         i += 1;
         if !item.starts_with('+') {
-            tmp.push(item);
+            pair[2 - i] = Some(item);
             continue;
         }
         match parse(item) {
             Err(source) => {
                 if Path::new(item).exists() {
-                    tmp.push(item);
+                    pair[2 - i] = Some(item);
                 } else {
                     return Err(Error::InvalidCoordinates {
                         coord: item.clone(),
@@ -34,11 +34,15 @@ pub(crate) fn edit(ctx: &Context, files: Vec<String>) -> Result<(), Error> {
                     write!(&mut coord_buf, " {}", item)?;
                 }
             }
-            Ok(_) => tmp.push(item),
+            Ok(_) => pair[2 - i] = Some(item),
         }
     }
 
-    for file in files[i..].iter().rev().chain(tmp.into_iter().rev()) {
+    for file in files[i..]
+        .iter()
+        .rev()
+        .chain(pair.into_iter().filter_map(|x| x))
+    {
         let p = std::fs::canonicalize(file).unwrap_or_else(|_| PathBuf::from(file));
         writeln!(&mut buf, "edit -existing '{}'", p.display())?;
     }
