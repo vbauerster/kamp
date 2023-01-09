6f012420af8966508fd89a1bda476250b9abdd8a
Vladimir Bauer <vbauerster@gmail.com>
2023-01-09 12:08:26 +0500

    use aliased Result<T>

--- src/kamp/cmd/kill.rs
+++ src/kamp/cmd/kill.rs
@@ -1,6 +1,6 @@
 use super::Context;
-use super::Error;
+use super::Result;
 
-pub(crate) fn kill(ctx: &Context, exit_status: Option<i32>) -> Result<(), Error> {
+pub(crate) fn kill(ctx: &Context, exit_status: Option<i32>) -> Result<()> {
     ctx.send_kill(exit_status)
 }
