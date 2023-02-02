c728a5791136f7e8c9c7a09208557eca5b9c9a42
Vladimir Bauer <vbauerster@gmail.com>
2023-02-02 20:18:23 +0500

    refactoring list

--- src/kamp/cmd/list.rs
+++ src/kamp/cmd/list.rs
@@ -1,5 +1,5 @@
+use super::Context;
 use super::Result;
-use super::{Context, SplitType};
 
 #[allow(unused)]
 #[derive(Debug)]
@@ -40,7 +40,7 @@ pub(crate) fn list_current(ctx: Context) -> Result<Session> {
 }
 
 fn to_session_struct(ctx: Context) -> Result<Session> {
-    ctx.query_val("client_list", SplitType::Kakoune, None)
+    ctx.query_val("client_list", false, true, None)
         .and_then(|clients| {
             clients
                 .iter()
@@ -48,13 +48,13 @@ fn to_session_struct(ctx: Context) -> Result<Session> {
                     let mut ctx_clone = ctx.clone();
                     ctx_clone.set_client(name);
                     ctx_clone
-                        .query_val("bufname", SplitType::none_quote_raw(), None)
+                        .query_val("bufname", false, false, None)
                         .map(|mut v| Client::new(name.into(), v.remove(0)))
                 })
                 .collect::<Result<Vec<Client>>>()
         })
         .and_then(|clients| {
-            ctx.query_sh("pwd", SplitType::none_quote_raw(), None)
+            ctx.query_sh("pwd", None)
                 .map(|mut v| Session::new(ctx.session().into_owned(), v.remove(0), clients))
         })
 }
