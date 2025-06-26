d268f19b73d73e1b49b7bc30786958d8cb543642
Vladimir Bauer <vbauerster@gmail.com>
2025-06-26 14:07:18 +0500

    attach: drop *stdin* workaround
    Previously on attach -b *stdin* buffers were accumulated so eval %sh
    workaround was applied. I didn't observe such issue with latest kakoune
    so it is ok to drop this workaround.

--- src/kamp/cmd/attach.rs
+++ src/kamp/cmd/attach.rs
@@ -4,8 +4,7 @@ use super::Result;
 pub(crate) fn attach(ctx: Context, buffer: Option<String>) -> Result<()> {
     let mut cmd = String::new();
     if let Some(buffer) = &buffer {
-        cmd.push_str(r#"eval %sh{ case "$kak_bufname" in (\*stdin*) echo delete-buffer ;; esac }"#);
-        cmd.push_str("\nbuffer '");
+        cmd.push_str("buffer '");
         cmd.push_str(buffer);
         cmd.push('\'');
     }
